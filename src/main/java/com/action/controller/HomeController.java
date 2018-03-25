ebdfc47c7f9c612ca3e6b1d8743b2c834314d0f6
biswajiit <biswajit.jmishra@gmail.com>
2018-03-25 20:09:41 +0530

    first commit

---
+++ src/main/java/com/action/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.action.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+	private static Logger LOGGER = Logger.getLogger(HomeController.class); 
+
+	@GetMapping(value="/detail")
+	public void getDetail() {
+		LOGGER.info("inside getDetail method call");
+	}
+}
