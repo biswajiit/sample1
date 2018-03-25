5b1d5476b1563ee97e83ffe899035a29afe9622b
biswajiit <biswajit.jmishra@gmail.com>
2018-03-25 20:50:21 +0530

    2nd Commit

--- src/main/java/com/action/controller/HomeController.java
+++ src/main/java/com/action/controller/HomeController.java
@@ -12,6 +12,6 @@ public class HomeController {
 
 	@GetMapping(value="/detail")
 	public void getDetail() {
-		LOGGER.info("inside getDetail method call");
+		LOGGER.info("inside getDetail method call..");
 	}
 }
